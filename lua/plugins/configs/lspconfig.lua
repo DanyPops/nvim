b4638267f4e5985c9185dbd031a0d69f9e3dfd99
Daniel Popsuevich <danypops@protonmail.com>
2025-07-23 18:07:56 +0300

    disable rust analyzer

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -69,7 +69,7 @@ lspconfig.lua_ls.setup {
 }
 
 -- setup multiple servers with same default options
-local servers = { "ts_ls", "html", "cssls", "gopls", "zls", "basedpyright", "clangd", "rust-analyzer"}
+local servers = { "ts_ls", "html", "cssls", "gopls", "zls", "basedpyright", "clangd"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
